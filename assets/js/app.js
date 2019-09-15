8cf547fde7763121e9dd6e38419e6375add39d81
Steven Hofheins <stevenisinsane@gmail.com>
2019-09-15 17:06:45 -0600

    created a new repository for this game so I could get the naming right

---
+++ assets/js/app.js
@@ -0,0 +1,68 @@
+//document ready function
+$( document ).ready(function() {
+
+
+//declaring variables
+var question;
+var answer1;
+var answer2;
+var answer3;
+var answer4;
+var correctAnswer;
+
+//creating questions
+
+var questionObj = [
+    {
+        question: "What activity will burn 150 calories?",
+        answers: {
+          a: "Banging your head on a wall for an hour",
+          b: "Untangling chords for thirty minutes",
+          c: "Meditating and thinking about running for ten minutes",
+          d: "Brushing your teeth for forty minutes"
+        },
+        correctAnswer: "a"
+      },
+
+      {
+        question: "What is a flock of crows called?",
+        answers: {
+          a: "A Bombardment",
+          b: "A Flock",
+          c: "A Murder",
+          d: "A Gaggle"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Who is the strongest?",
+        answers: {
+          a: "Gorilla",
+          b: "Dung Beetle",
+          c: "Anaconda",
+          d: "Penguin"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Which Animal is the Loudest?",
+        answers: {
+          a: "Doggo",
+          b: "Elephant",
+          c: "Lion",
+          d: "Whale"
+        },
+        correctAnswer: "d"
+      },
+]
+
+//populating questions to html
+
+//creating timer 
+
+//capturing wins and losses
+
+//report results at the end (probably use the capturing wins and losses function and add them together)
+
+//end doc ready function
+});
